22329c6020c2306dbd9bd1ddbd83e8af8635fe46
Noor McVay <38887557+NoreMcVay@users.noreply.github.com>
2019-04-13 17:25:06 +0100

    Add files via upload

---
+++ meanFolder/backend/app.js
@@ -0,0 +1,34 @@
+// const path = require("path");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const postsRoutes = require('./routes/posts');
+
+const app = express();
+
+const mongodb = 'mongodb://localhost:27017/meanApp';
+
+const getImage = require('path').join(__dirname,'/images');
+
+mongoose.connect(mongodb, {useNewUrlParser: true}).then(() => {
+  console.log("CONNECTED.")
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/images", express.static(getImage)); //made this up !!!!!
+
+//app.use(express.static(path.join(__dirname, '../meanApp/dist/meanApp'))); this is to get it working on localhost3000!!!!!!!
+
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-Width, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
+    next();
+});
+
+app.use('/api/posts', postsRoutes);
+
+module.exports = app;
---
+++ meanFolder/meanApp/src/app/app.module.ts
@@ -0,0 +1,55 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+import {
+  MatInputModule,
+  MatCardModule,
+  MatButtonModule,
+  MatToolbarModule,
+  MatExpansionModule,
+  MatProgressSpinnerModule,
+  MatPaginatorModule
+} from '@angular/material';
+
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { HeaderComponent } from './header/header.component';
+import { ErrorComponent } from './error/error.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostCreateComponent,
+    PostListComponent,
+    HeaderComponent,
+    ErrorComponent,
+    LoginComponent,
+    SignupComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatProgressSpinnerModule,
+    HttpClientModule,
+    MatPaginatorModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
